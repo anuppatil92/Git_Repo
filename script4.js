ecbb06513abd909c7627ab02b894042ddaef91a2
anuppatil <patil.anup92@gmail.com>
2024-02-12 17:33:15 +0530

    Pass Generator

---
+++ script4.js
@@ -0,0 +1,25 @@
+var password= document.getElementById('password')
+function genPassword() {
+    var chars= '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
+    var passwordLength=12
+    var password=''
+    for (var i=0; i<passwordLength; i++){
+        var randomNumber= Math.floor(Math.random() * chars.length)
+        password= chars.substring(randomNumber, randomNumber+1)
+    }
+    document.getElementById('password').value= password
+}
+
+function copyPassword() {
+    var copyText = document.getElementById('password') 
+    copyText.select();
+    copyText.setSelectionRange(0,999)
+    document.execCommand('copy')
+}
+
+/*
+let btn1= document.getElementById("bt1");
+btn1.addEventListener("click", genPassword);
+let btn2= document.getElementById("bt2");
+btn2.addEventListener("click", copyPassword);
+*/
